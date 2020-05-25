af828373001a3ff0c9f5160fc715c73f05abb155
rzfvansuijdam <55277122+rzfvansuijdam@users.noreply.github.com>
2020-05-25 09:57:07 +0200

    Add files via upload

---
+++ scripts/kill_script.cs
@@ -0,0 +1,25 @@
+﻿ using UnityEngine;
+ using System.Collections;
+
+ public class kill_script : MonoBehaviour 
+ {
+
+
+     // Use this for initialization
+    void Start () 
+    {
+
+    }
+
+     // Update is called once per frame
+     void Update () 
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        // Destroy game object
+        Destroy (this.gameObject);
+    }
+ }
---
+++ scripts/waveUIscript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+using UnityEngine.UI;
+public class waveUIscript : MonoBehaviour
+{
+
+    [SerializeField]
+    EnemySpawner spawner;
+ 
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
